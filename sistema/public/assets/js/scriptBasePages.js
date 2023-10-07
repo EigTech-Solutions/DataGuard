18ac32800753bc4ec3e7a676905960663c73a468
Kauanny Tenorio <kakau24112004@hotmail.com>
2023-10-06 21:44:13 -0300

    deixando a estilização do menu lateral dinâmico

--- sistema/public/assets/js/scriptBasePages.js
+++ sistema/public/assets/js/scriptBasePages.js
@@ -1,37 +1,35 @@
 var perfUsuario = "Técnico";
 
 // Recupera a página atual armazenada no localStorage
-var pageAtual = localStorage.getItem('pageAtual') || 'bntDashMenu';
+var pageAtual = sessionStorage.PAGE_ATUAL;
 
-// var pageAtual = sessionStorage.getItem('paginaAtual') || 'DashboardGeral.html';
-// atualizarPageAtualMenu(pageAtual);
 
 menuLateral.innerHTML = `
     <img src="../assets/images/Logo.png" class="logo" alt="Logo">
 
     <ul id="menuList">
         <li>
-            <a href="DashboardGeral.html" id="bntDashMenu" class="menu-link" onclick="atualizarPageAtualMenu(this)">
+            <a href="DashboardGeral.html" id="bntDashMenu" class="menu-link" onclick="atualizarPageAtualMenu(this.id)">
                 <img src="../assets/images/dashboard.png" width="37px" alt="icon dashboard"> Dashboard
             </a>
         </li>
         <li>
-            <a href="teste.html" id="bntLabMenu" class="menu-link" onclick="atualizarPageAtualMenu(this)">
+            <a href="teste.html" id="bntLabMenu" class="menu-link" onclick="atualizarPageAtualMenu(this.id)">
                 <img src="../assets/images/carbon_labs.png" alt="icon laboratório"> Laboratórios
             </a>
         </li>
         <li>
-            <a href="#" id="bntPCsMenu" class="menu-link" onclick="atualizarPageAtualMenu(this)">
+            <a href="#" id="bntPCsMenu" class="menu-link" onclick="atualizarPageAtualMenu(this.id)">
                 <img src="../assets/images/icon_computer-outline.png" alt="icon PC"> Computadores
             </a>
         </li>
         <li>
-            <a href="#" id="bntUsersMenu" class="menu-link" onclick="atualizarPageAtualMenu(this)">
+            <a href="#" id="bntUsersMenu" class="menu-link" onclick="atualizarPageAtualMenu(this.id)">
                 <img src="../assets/images/iwwa_user.png" alt="icon usuários"> Usuários
             </a>
         </li>
         <li>
-            <a href="#" id="bntConfigMenu" class="menu-link" onclick="atualizarPageAtualMenu(this)">
+            <a href="#" id="bntConfigMenu" class="menu-link" onclick="atualizarPageAtualMenu(this.id)">
                 <img src="../assets/images/carbon_settings-edit.png" alt=""> Configurações
             </a>
         </li>
@@ -56,9 +54,7 @@ menuTop.innerHTML = `
     </button>
 `;
 
-// Restaura a classe pgAtual na página atual ao iniciar
-document.getElementById('bntDashMenu').classList.add('pgAtual');
-
+atualizarPageAtualMenu(pageAtual);
 exibirMenuPerfil(perfUsuario);
 
 function atualizarPerfilExibicao() {
@@ -78,22 +74,17 @@ function exibirMenuPerfil(perfUsuario) {
     }    
 }
 
-function atualizarPageAtualMenu(page) {
+function atualizarPageAtualMenu(pageAtual) {
+    var elementoPageAtual = document.querySelector('#' + pageAtual);
+
     // Remove a classe pgAtual de todos os links do menu
     const menuLinks = document.querySelectorAll('.menu-link');
     menuLinks.forEach(link => link.classList.remove('pgAtual'));
 
     // Adiciona a classe pgAtual ao link clicado
-    page.classList.add('pgAtual');
+    elementoPageAtual.classList.add('pgAtual');
 
-    // Atualiza a variável pageAtual com base no texto do link clicado
-    // pageAtual = page;
-    // Armazena a página atual no armazenamento local
-    // sessionStorage.setItem('paginaAtual', page.href);
-
-    // Atualiza a variável pageAtual e armazena no localStorage
-    pageAtual = page.id;
-    localStorage.setItem('pageAtual', pageAtual);
+    sessionStorage.PAGE_ATUAL = elementoPageAtual.id;
 }
 
 function logout() {
