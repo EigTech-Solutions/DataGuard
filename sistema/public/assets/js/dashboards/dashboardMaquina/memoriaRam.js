2fdbb8efd81ee445d0defb8c29859474ce5e14bf
InacioFigueiredo13 <inaciofigueiredo13@gmail.com>
2023-10-09 20:37:04 -0300

    Finalizando gráfico de RAM

--- sistema/public/assets/js/dashboards/dashboardMaquina/memoriaRam.js
+++ sistema/public/assets/js/dashboards/dashboardMaquina/memoriaRam.js
@@ -1,5 +1,6 @@
 const div_parteUtilizada = document.getElementById("memoria_ram_bar_filled");
 const span_memoria_ram_porcentagem_valor = document.getElementById("memoria_ram_porcentagem");
 
-let numAleatorio = Math.random() * 101;
-
+let numAleatorio = Math.random() * 100;
+div_parteUtilizada.style.width = numAleatorio + "%";
+span_memoria_ram_porcentagem_valor.innerHTML = Math.ceil(numAleatorio) + "%"
