61e7d5874a2a3e9671261a6b6f359ca69c17a80f
Cabayer915 <cauaribeirob915@gmail.com>
2023-11-23 14:15:54 -0300

    Front-End finalizado Tela Instituição

--- sistema/public/assets/js/instituicao.js
+++ sistema/public/assets/js/instituicao.js
@@ -73,6 +73,7 @@ function cadastrar() {
         });
 
     }
+    location.reload();
 }
 function abrirModalCardastarInstituicao() {
     divModal.style.display = "flex";
@@ -142,6 +143,7 @@ function mostrarDados(dados) {
         totalClasse.innerHTML = `${totalInstituicoes}`
     }
 }
+
 function dadosInstituicao() {
     fetch(`/instituicao/dadosInstituicao`, { cache: 'no-store' }).then(function (response) {
         if (response.ok) {
@@ -163,36 +165,99 @@ dadosInstituicao()
 
 
 function mostrarInformacao(informacao) {
+    blocoDeDado.innerHTML = '';
+
     for (i = 0; i < informacao.length; i++) {
         var nomeInstituicao = informacao[i].nomeInstitucional;
         blocoDeDado.innerHTML += 
-        `<div class="Instituicoes">
+        `<div class="Instituicoes" data-nome="${nomeInstituicao}">
             ${nomeInstituicao}
-         </div>`
-        console.log("for")
+         </div>`;
+        console.log("for");
     }
+}
+
+function pesquisarInstituicao() {
+    var busca = input_busca.value.toLowerCase();
+
+    var instituicoes = document.querySelectorAll('.Instituicoes');
+
+    instituicoes.forEach(function (instituicao) {
+        var nome = instituicao.getAttribute('data-nome').toLowerCase();
+
+        if (nome.includes(busca)) {
+            instituicao.style.display = 'block';
+        } else {
+            instituicao.style.display = 'none';
+        }
+    });
 }
 
 function dadosGeraisInst() {
     fetch(`/instituicao/dadosGeraisInst`, { cache: 'no-store' }).then(function (response) {
         if (response.ok) {
             response.json().then(function (dadosPuxados) {
-                console.log("Response -> " + response[0].cnpj)
-                console.log("DadosPuxados -> " + dadosPuxados[0].cnpj)
-                mostrarInf(dadosPuxados)
-
+                // console.log("Response -> " + response[0].cnpj)
+                // console.log("DadosPuxados -> " + dadosPuxados[0].cnpj)
+                abrirModalExbirInfosDetalhadas(dadosPuxados); // Passa os dados para a função
             });
         } else {
-            console.error('Nenhuma informaçao encontrada ou erro na API');
+            console.error('Nenhuma informação encontrada ou erro na API');
         }
     })
-        .catch(function (error) {
-            console.error(`Erro na obtenção dos dados: ${error.message}`);
-        });
+    .catch(function (error) {
+        console.error(`Erro na obtenção dos dados: ${error.message}`);
+    });
 }
-dadosGeraisInst()
 
-function mostrarInf(dadosPuxados) {
+function abrirModalExbirInfosDetalhadas(dadosPuxados) {
+    document.body.classList.add('modal-open');
+
+    // Criar a sobreposição do modal
+    const overlay = document.createElement('div');
+    overlay.className = 'modal-overlay';
+    document.body.appendChild(overlay);
+    divModalDetalhado.style.display = "flex";
+
+    divModalDetalhado.innerHTML = `
+    <!-- Modal Detalhado -->
+    <div id="modalDetalhado">
+      <div class="modal-header">
+        <h2>Informações das Instituições</h2>
+        <button onclick="fecharModalDetalhado()" id="close-modal">Fechar</button>
+      </div>
+      <div class="titulo-rotulo">
+      <div class="modal-body">
+        <p>
+          Nome
+        </p>
+        <p>
+          CNPJ
+        </p>
+        <p>
+          Email
+        </p>
+        <p>
+          Telefone
+        </p>
+        <p>
+          CEP
+        </p>
+        <p>
+          Número do Endereço
+        </p>
+        <p>
+          Complemento
+        </p>
+      </div>
+      </div>
+      <div class="scrollDoModal">
+      <div id="blocoDeDados"></div>
+      </div>
+    </div>
+    `;
+    blocoDeDados.innerHTML = '';
+    console.log("eeeeeee");
     for (i = 0; i < dadosPuxados.length; i++) {
         var nomeInstituicao = dadosPuxados[i].nomeInstitucional;
         var cnpj = dadosPuxados[i].cnpj;
@@ -201,30 +266,29 @@ function mostrarInf(dadosPuxados) {
         var cep = dadosPuxados[i].cep;
         var numeroEndereco = dadosPuxados[i].numeroEndereco;
         var complemento = dadosPuxados[i].complemento;
-
-        todasInst.innerHTML += `
-                        <div>
-                            <p>Nome: ${nomeInstituicao}</p>
-                            <p>CNPJ: ${cnpj}</p>
-                            <p>Email: ${email}</p>
-                            <p>Telefone: ${telefone}</p>
-                            <p>CEP: ${cep}</p>
-                            <p>Número do Endereço: ${numeroEndereco}</p>
-                            <p>Complemento: ${complemento}</p>
-                        </div>
-                    `;
-                    console.log("inner")
+    
+        blocoDeDados.innerHTML += `
+        <div class="modal-body-info">
+          <p>${nomeInstituicao}</p>
+          <p class="tom">${cnpj}</p>
+          <p class="emailModal">${email}</p>
+          <p class="tom">${telefone}</p>
+          <p>${cep}</p>
+          <p class="tom">${numeroEndereco}</p>
+          <p>${complemento}</p>
+        </div>`;
     }
+    
 }
 
-function pesquisarInstituicao(dadosPuxados){
-    var busca = input_busca.value;
+function fecharModalDetalhado() {
+    document.body.classList.remove('modal-open');
 
-    for (i = 0; i < dadosPuxados.length; i++) {
-        var nomeInstituicao = dadosPuxados[i].nomeInstitucional;
-        var nomes = []
-        nomes.push(nomeInstituicao);
-        console.log(nomes)
-
+    // Remover a sobreposição do modal
+    const overlay = document.querySelector('.modal-overlay');
+    if (overlay) {
+        overlay.parentNode.removeChild(overlay);
     }
+
+    divModalDetalhado.style.display = "none";
 }
